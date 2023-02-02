c7d17c1c73db2b0b648179bd39a2db2afee2c0f7
SangJo Kim <kandrea@nate.com>
2023-02-02 14:44:23 +0900

    tuning nTune

--- selfdrive/ntune.py
+++ selfdrive/ntune.py
@@ -254,19 +254,19 @@ class nTune():
     if self.checkValue("stoppingDecelRate", 0.4, 3.0, 3.0):
       updated = True
 
-    if self.checkValue("vEgoStopping", 0.4, 1.0, 0.3):
+    if self.checkValue("vEgoStopping", 0.4, 1.0, 0.6):
       updated = True
 
-    if self.checkValue("vEgoStarting", 0.4, 1.0, 0.25):
+    if self.checkValue("vEgoStarting", 0.4, 1.0, 0.5):
       updated = True
 
     if self.checkValue("stopAccel", -3.0, -2.0, -2.0):
       updated = True
 
-    if self.checkValue("STOP_DISTANCE", 4.5, 7.0, 6.0):
+    if self.checkValue("STOP_DISTANCE", 4.5, 7.0, 5.0):
       updated = True
 
-    if self.checkValue("COMFORT_BRAKE", 2.0, 3.0, 2.5):
+    if self.checkValue("COMFORT_BRAKE", 2.0, 3.0, 2.0):
       updated = True
 
     if self.checkValue("StopAtStopSign", 0., 1., 1.):
