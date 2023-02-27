40ea4d676b3dec358f83306a1c3529901477e64c
SangJo Kim <kandrea@nate.com>
2023-02-27 13:47:54 +0900

    bluetooth connect

--- selfdrive/road_speed_limiter.py
+++ selfdrive/road_speed_limiter.py
@@ -244,7 +244,12 @@ def main():
 
   with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
     try:
-      sock.bind(('0.0.0.0', Port.RECEIVE_PORT))
+
+      try:
+        sock.bind(('0.0.0.0', 843))
+      except:
+        sock.bind(('0.0.0.0', Port.RECEIVE_PORT))
+
       sock.setblocking(False)
 
       while True:
