8eb12802148e40d21b2458f3c969f4b321b93a38
SangJo Kim <kandrea@nate.com>
2023-06-11 22:48:51 +0900

    Update gmcan.py

--- selfdrive/car/gm/gmcan.py
+++ selfdrive/car/gm/gmcan.py
@@ -87,9 +87,9 @@ def create_friction_brake_command(packer, bus, apply_brake, idx, enabled, near_s
 
   return packer.make_can_msg("EBCMFrictionBrakeCmd", bus, values)
 
-
-def create_acc_dashboard_command(packer, bus, enabled, target_speed_kph, lead_car_in_sight, fcw, follow_level):
-  target_speed = min(target_speed_kph, 255)
+def create_acc_dashboard_command(packer, bus, acc_engaged, target_speed_kph, lead_car_in_sight, fcw, follow_level):
+  # Not a bit shift, dash can round up based on low 4 bits.
+  target_speed = int(target_speed_kph * 16) & 0xfff
 
   values = {
     "ACCAlwaysOne": 1,
